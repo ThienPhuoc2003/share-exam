1fdea609e3cfe2d4f6cc9f6a30ca4b260180843a
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2024-07-20 12:50:24 +0700

    26:14

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+
+export default {
+    providers: [
+      {
+        domain: "https://communal-ringtail-89.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/app/ConvexClientProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ReactNode } from "react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export function ConvexClientProvider({ children }: { children: ReactNode }) 
+{
+    return ( // Make sure the opening parenthesis is on the same line as return
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
